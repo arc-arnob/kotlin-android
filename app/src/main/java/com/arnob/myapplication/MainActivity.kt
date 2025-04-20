9a071d629173c3896f635b838b011d848b5c5073
Arnob  Chowdhury <arnobchaudhury@gmail.com>
2025-04-20 11:11:44 +0200

    Add click events, Handling text change

--- app/src/main/java/com/arnob/myapplication/MainActivity.kt
+++ app/src/main/java/com/arnob/myapplication/MainActivity.kt
@@ -2,6 +2,8 @@ package com.arnob.myapplication
 
 import android.os.Binder
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
@@ -16,11 +18,31 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding =  ActivityMainBinding.inflate(layoutInflater)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
 
         val textView = binding.textView
         val editText = binding.editText
         val button = binding.button
+
+        binding.button.setOnClickListener{
+            val name = binding.editText.text.toString()
+            binding.textView.text = "Hello $name!"
+        }
+
+        binding.editText.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                // Called before text changes
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                // Called during text changes
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                // Called after text changes
+                binding.textView.text = "Typing: ${s.toString()}"
+            }
+        })
 
     }
 }
