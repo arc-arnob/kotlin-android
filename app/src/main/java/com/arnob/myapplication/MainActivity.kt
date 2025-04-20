d8ea9aa6cd8ac961c2dcf10ae3d7641ea31ab13e
Arnob  Chowdhury <arnobchaudhury@gmail.com>
2025-04-20 11:03:35 +0200

    Add View Binding

--- app/src/main/java/com/arnob/myapplication/MainActivity.kt
+++ app/src/main/java/com/arnob/myapplication/MainActivity.kt
@@ -1,21 +1,26 @@
 package com.arnob.myapplication
 
+import android.os.Binder
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.arnob.myapplication.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding =  ActivityMainBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_main)
 
-        // Find views by their IDs
-        val textView = findViewById<TextView>(R.id.textView)
-        val editText = findViewById<EditText>(R.id.editText)
-        val button = findViewById<Button>(R.id.button)
+        val textView = binding.textView
+        val editText = binding.editText
+        val button = binding.button
+
     }
 }
