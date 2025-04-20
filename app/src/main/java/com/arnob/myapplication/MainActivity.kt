d17aeb96d54ec4ae1dd3d2532cf3b5f37704fddc
Arnob  Chowdhury <arnobchaudhury@gmail.com>
2025-04-20 10:58:37 +0200

    Add Find view by id

--- app/src/main/java/com/arnob/myapplication/MainActivity.kt
+++ app/src/main/java/com/arnob/myapplication/MainActivity.kt
@@ -1,14 +1,21 @@
 package com.arnob.myapplication
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // Your code starts here
+        // Find views by their IDs
+        val textView = findViewById<TextView>(R.id.textView)
+        val editText = findViewById<EditText>(R.id.editText)
+        val button = findViewById<Button>(R.id.button)
     }
 }
